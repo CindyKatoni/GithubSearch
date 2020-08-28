49458f4c273cece237a1170d0d6e21c1cf41c839
CindyKatoni <katmuema@gmail.com>
2020-08-28 22:44:24 +0300

    getName function in GithubService created

--- src/app/github.service.ts
+++ src/app/github.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { User } from './user';
 import { Repository } from './repository';
 import { environment } from '../environments/environment';
@@ -8,6 +9,16 @@ import { environment } from '../environments/environment';
 })
 export class GithubService {
   name: string;
+
+  constructor(private _http: HttpClient) {
+    console.log('Ready to use github service...');
+    this.name =  'CindyKatoni';
+  }
+
+  getName() {
+    const user = this._http.get(`https://api.github.com/users/${this.name}`)
+    console.log(user);
+  }
+
   
-  constructor() { }
 }
