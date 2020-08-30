df5c81d19dd3207f079da73f45cd4e4d2cf8b8e3
CindyKatoni <katmuema@gmail.com>
2020-08-30 22:25:00 +0300

    hide environment file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {GithubService} from './github.service';
+import { SearchformComponent } from './searchform/searchform.component';
 
 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [GithubService]
 })
 export class AppComponent {
   
